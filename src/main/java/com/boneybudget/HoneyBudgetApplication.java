610be8065076c0a5b85f80198d2158879fc96d07
cdcpp <oblilic93@gmail.com>
2026-09-13 20:26:46 +0900

    chore: Spring Boot 초기 프로젝트 세팅

---
+++ src/main/java/com/boneybudget/HoneyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.boneybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boneybudget/HoneyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boneybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
